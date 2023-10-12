48e433aa969b2f7847c15841488953f8e5f1c370
zubinsood <zubinsood@icloud.com>
2023-10-12 15:46:10 -0400

    Merged work on Dev Branch

--- src/components/HorizontalScrollItem.tsx
+++ src/components/HorizontalScrollItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTheme } from '../contexts/ThemeContext';
 
 interface ScrollItemProps {
   imageUrl: string;
@@ -6,8 +7,9 @@ interface ScrollItemProps {
 }
 
 const HorizontalScrollItem: React.FC<ScrollItemProps> = ({ imageUrl, text }) => {
+  const { isToggled } = useTheme();
   return (
-    <div className="scroll-item">
+    <div className={`scroll-item ${isToggled ? 'dark' : 'light'}`}>
       <div className="scroll-item-img-container">
         <img className="scroll-item-img" src={imageUrl}></img>
       </div>
