4405910c6f73b876ab092b23a9fe6d1f00afc461
zubinsood <zubinsood@icloud.com>
2023-10-13 10:59:33 -0400

    No current issues

--- src/components/Drinks.tsx
+++ src/components/Drinks.tsx
@@ -12,18 +12,19 @@ const Drinks: React.FC<DrinkProps> = ({buttonText}) => {
       <div className="card">
         <div className="row">
           <div className="image-1" />
-          <div>
+          <div className="row-container">
             <div className='drink-name'>Samurai Mojito</div>
       
             <div className="description">
               Refreshing original blend of Japanese Shiso, Yuzu, Sake, and Lemon.
             </div> <br/>
+
             <Button buttonText="Add to cart"/>
           </div>
         </div>
         <div className="row">
           <div className="image-2" />
-          <div>
+          <div className="row-container">
             <div className='drink-name'>Hojicha Latte</div>
             <div className="description">
               A velvety, hand-sifted fan favorite that is perfect for cozy weather.
@@ -33,7 +34,7 @@ const Drinks: React.FC<DrinkProps> = ({buttonText}) => {
         </div>
         <div className="row">
           <div className="image-3" />
-          <div>
+          <div className="row-container">
             <div className='drink-name1'>Maple Latte</div>
             <div className="description1">
               A soft and sweet treat! Treat yourself to our creamy Maple Latte with our staple toasts.
