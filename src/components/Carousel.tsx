2f9d4beef779e52f5141b67bc33f926922b90fb9
zubinsood <zubinsood@icloud.com>
2023-10-13 10:01:20 -0400

    NavBar complete

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -9,7 +9,6 @@ const images = [
     '/images/carousel-1.png',
     '/images/carousel-2.png',
     '/images/carousel-3.png',
-    '/images/carousel-1.png', // Duplicate the first image
 ];
 
 function CarouselIndicator({ currentIndex }: CarouselIndicatorProps) {
@@ -25,43 +24,29 @@ function CarouselIndicator({ currentIndex }: CarouselIndicatorProps) {
 
 function Carousel() {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
-    const [transitionEnabled, setTransitionEnabled] = useState(true);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            if (currentImageIndex === images.length - 2) {
-                // Transition to the duplicated first image
-                setCurrentImageIndex(prevIndex => prevIndex + 1);
-            } else if (currentImageIndex === images.length - 1) {
-                // After a very short delay, jump to the actual first image without transition
-                setTimeout(() => {
-                    setTransitionEnabled(false);
-                    setCurrentImageIndex(0);
-                    // Re-enable the transition for the next cycle
-                    setTimeout(() => setTransitionEnabled(true), 10);
-                }, 10);
-            } else {
-                setCurrentImageIndex(prevIndex => prevIndex + 1);
-            }
+            setCurrentImageIndex(prevIndex => (prevIndex + 1) % images.length);
         }, 1000);
-
         return () => clearInterval(interval);
-    }, [currentImageIndex]);
+    }, [images.length]);
 
     const offset = -currentImageIndex * 100;
 
     return (
         <div className="carousel">
-            <div style={{ display: 'flex', transform: `translateX(${offset}%)`, transition: transitionEnabled ? 'transform 0.25s ease-in' : 'none' }}>
-                {images.map((image, index) => (
-                    <img key={index} src={image} alt={`carousel-${index}`} />
-                ))}
-            </div>
+            {images.map((image, index) => (
+                <img 
+                    key={index} 
+                    src={image} 
+                    alt={`carousel-${index + 1}`}
+                    className={`${index === currentImageIndex ? 'active' : ''} ${index === ((currentImageIndex - 1 + images.length) % images.length) ? 'prev' : ''}`}
+                />
+            ))}
             <CarouselIndicator currentIndex={currentImageIndex} />
         </div>
     );
 }
-
-
 
 export default Carousel;
